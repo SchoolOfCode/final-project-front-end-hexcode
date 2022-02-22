7c03a3b20edfa471cc3e2c851e6a8ab977b3628c
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-02-22 16:19:07 +0000

    create event section started

--- src/components/CreateEventSection/index.js
+++ src/components/CreateEventSection/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React from 'react';
+import TitleSection from '../TitleSection';
+import PeopleSection from '../PeopleSection';
+import LocationSection from '../LocationSection';
+import DateSection from '../DateSection';
 
 function CreateEventSection() {
-  return <div>CreateEventSection</div>;
+  return (
+    <div>
+      <h2>Create an Event</h2>
+      <div>
+        <h3>Title</h3>
+        <input type='text' />
+      </div>
+      <div>
+        <h3>People</h3>
+        <input type='text' />
+      </div>
+      <div>
+        <h3>Date</h3>
+        <input type='text' />
+      </div>
+      <div>
+        <h3>Time</h3>
+        <input type='text' />
+      </div>
+      <p></p>
+      <div>
+        <h3>Description</h3>
+        <input type='text' />
+      </div>
+    </div>
+  );
 }
 
 export default CreateEventSection;
--- src/pages/CreateEvent.js
+++ src/pages/CreateEvent.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React from 'react';
+import CreateEventSection from '../components/CreateEventSection';
 
 function CreateEvent() {
-  return <div>CreateEvent</div>;
-  <p>
-    If you havent decided on a date or location dont worry, you can decide this
-    later by adding a poll on the event and editing the event details once
-    decided.
-  </p>;
+  return (
+    <div>
+      <CreateEventSection />
+
+      <p>
+        If you havent decided on a date or location dont worry, you can decide
+        this later by adding a poll on the event and editing the event details
+        once decided.
+      </p>
+      <button>Confirm Event</button>
+    </div>
+  );
 }
 
 export default CreateEvent;
