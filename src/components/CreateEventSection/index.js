e0a3a9712dc86ea0be3b19766dbb15653cd7b28a
jamesgaynor <james_gaynor@btinternet.com>
2022-02-23 12:30:48 +0000

    created event/date/time states

--- src/components/CreateEventSection/index.js
+++ src/components/CreateEventSection/index.js
@@ -17,14 +17,27 @@ function CreateEventSection() {
   const [event, setEvent] = useState({
     title: "",
     people: [],
-    date: "Date pending",
-    time: "",
+    location: "",
     description: "",
   });
-  console.log("test");
-  // Handle change function for input fields on form
-  function handleChange(e) {}
 
+  const [eventDate, setEventDate] = useState("date pending");
+  const [eventTime, setEventTime] = useState("");
+
+  // Handle change function for input fields on form
+  function onChange(date, dateString) {
+    setEventDate(dateString);
+  }
+  function onChangeTime(date, timeString) {
+    setEventTime(timeString);
+  }
+  function handleChange(e) {
+    const value = e.target.value;
+    //const selected = e.target.selected;
+    setEvent({ ...event, [e.target.name]: value });
+    console.log(event.title);
+  }
+  console.log("this is the enet", event, eventDate, eventTime);
   // Ant components stuff
   const { TextArea } = Input;
   const menu = (
@@ -47,7 +60,21 @@ function CreateEventSection() {
       <form>
         <label>
           <h3>Title</h3>
-          <Input placeholder="Set a title for your event" />
+          <Input
+            placeholder="Set a title for your event"
+            name="title"
+            value={event.title}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          <h3>Location</h3>
+          <Input
+            placeholder="Set a location for your event"
+            name="location"
+            value={event.location}
+            onChange={handleChange}
+          />
         </label>
         <label>
           <h3>People</h3>
@@ -63,15 +90,26 @@ function CreateEventSection() {
         </label>
         <label>
           <h3>Date</h3>
-          <DatePicker />
+          <DatePicker name="date" selected={event.date} onChange={onChange} />
         </label>
         <label>
           <h3>Time</h3>
-          <TimePicker />
+          <DatePicker
+            picker="time"
+            name="time"
+            selected={event.time}
+            onChange={onChangeTime}
+          />
         </label>
         <label>
           <h3>Description</h3>
-          <TextArea placeholder="Add a description for your event.." autoSize />
+          <TextArea
+            placeholder="Add a description for your event.."
+            autoSize
+            name="description"
+            value={event.description}
+            onChange={handleChange}
+          />
           <div style={{ margin: "24px 0" }} />
         </label>
       </form>
