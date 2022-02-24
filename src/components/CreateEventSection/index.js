07b267cad426a1c2c34a9ef48f5ad329b5049ca3
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-02-24 15:57:17 +0000

    post working

--- src/components/CreateEventSection/index.js
+++ src/components/CreateEventSection/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import {
   DatePicker,
   Input,
@@ -9,20 +9,51 @@ import {
   Button,
   Space,
   message,
-} from "antd";
-import { UserOutlined } from "@ant-design/icons";
-import "antd/dist/antd.css";
+} from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
 
 function CreateEventSection() {
   // States
   const [event, setEvent] = useState({
-    eventTitle: "",
+    eventTitle: '',
     people: [],
-    eventLocation: "",
-    eventDescription: "",
+    eventLocation: '',
+    eventDescription: '',
   });
-  const [eventDate, setEventDate] = useState("date pending");
-  const [eventTime, setEventTime] = useState("");
+  const [eventDate, setEventDate] = useState('date pending');
+  const [eventTime, setEventTime] = useState('');
+
+  function postData() {
+    async function createEvent() {
+      const newEvent = {
+        organiserId: 2,
+        eventTitle: event.eventTitle,
+        eventDescription: event.eventDescription,
+        eventLocation: event.eventLocation,
+        eventTime: eventTime,
+        eventRequirements: 'Booze',
+        eventCategory: 'Drinks',
+      };
+      console.log(newEvent);
+      const response = await fetch(
+        'https://hexcode-arrange-group-event.herokuapp.com/events/',
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(newEvent),
+        }
+      );
+
+      const data = await response.json();
+      console.log({ data });
+    }
+    createEvent();
+  }
+
+  function handleClick() {
+    postData();
+  }
 
   // Handle change function for input fields on form
   function onChange(date, dateString) {
@@ -48,27 +79,27 @@ function CreateEventSection() {
   const { TextArea } = Input;
   const menu = (
     <Menu>
-      <Menu.Item key="Belinda" icon={<UserOutlined />}>
+      <Menu.Item key='Belinda' icon={<UserOutlined />}>
         Belinda
       </Menu.Item>
-      <Menu.Item key="Luke" icon={<UserOutlined />}>
+      <Menu.Item key='Luke' icon={<UserOutlined />}>
         Luke
       </Menu.Item>
-      <Menu.Item key="James" icon={<UserOutlined />}>
+      <Menu.Item key='James' icon={<UserOutlined />}>
         James
       </Menu.Item>
     </Menu>
   );
 
   return (
-    <div className="formContainer">
+    <div className='formContainer'>
       <h2>Create an Event</h2>
       <form>
         <label>
           <h3>Title</h3>
           <Input
-            placeholder="Set a title for your event"
-            name="eventTitle"
+            placeholder='Set a title for your event'
+            name='eventTitle'
             value={event.eventTitle}
             onChange={handleChange}
           />
@@ -76,8 +107,8 @@ function CreateEventSection() {
         <label>
           <h3>Location</h3>
           <Input
-            placeholder="Set a location for your event"
-            name="eventLocation"
+            placeholder='Set a location for your event'
+            name='eventLocation'
             value={event.eventLocation}
             onChange={handleChange}
           />
@@ -87,7 +118,7 @@ function CreateEventSection() {
           <Space wrap>
             <Dropdown.Button
               overlay={menu}
-              placement="bottomCenter"
+              placement='bottomCenter'
               icon={<UserOutlined />}
             >
               Add people
@@ -96,11 +127,11 @@ function CreateEventSection() {
         </label>
         <label>
           <h3>Date</h3>
-          <DatePicker name="date" onChange={onChange} />
+          <DatePicker name='date' onChange={onChange} />
         </label>
         <label>
           <h3>Time</h3>
-          <DatePicker picker="time" name="time" onChange={onChangeTime} />
+          <DatePicker picker='time' name='time' onChange={onChangeTime} />
         </label>
         <p>
           If you havent decided on a date or location dont worry, you can decide
@@ -110,16 +141,18 @@ function CreateEventSection() {
         <label>
           <h3>Description</h3>
           <TextArea
-            placeholder="Add a description for your event.."
+            placeholder='Add a description for your event..'
             autoSize
-            name="eventDescription"
+            name='eventDescription'
             value={event.eventDescription}
             onChange={handleChange}
           />
-          <div style={{ margin: "24px 0" }} />
+          <div style={{ margin: '24px 0' }} />
         </label>
-        <Link to="/Event">
-          <Button type="primary">Create Event</Button>
+        <Link to='/Event'>
+          <Button type='primary' onClick={handleClick}>
+            Create Event
+          </Button>
         </Link>
       </form>
     </div>
