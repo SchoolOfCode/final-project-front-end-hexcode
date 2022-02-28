28571f9f2ff220065a28dd76bd85e93bc5df21f5
jamesgaynor <james_gaynor@btinternet.com>
2022-02-28 14:42:14 +0000

    login router linked to homepage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //import Navbar from '../Nabvar';
-import CreateEvent from '../../pages/CreateEvent';
-import Event from '../../pages/Event';
-import 'antd/dist/antd.css';
-import LoginPage from '../../pages/LoginPage';
+import CreateEvent from "../../pages/CreateEvent";
+import Event from "../../pages/Event";
+import "antd/dist/antd.css";
+import LoginPage from "../../pages/LoginPage";
+import HomePage from "../../pages/homePage";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/homepage' />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/homepage" element={<HomePage />} />
 
-          <Route path='/createEvent' element={<CreateEvent />} />
-          <Route path='/event/:id' element={<Event />} />
+          <Route path="/createEvent" element={<CreateEvent />} />
+          <Route path="/event/:id" element={<Event />} />
           {/* <Route path='/' />
           <Route path='/createEvent' element={<CreateEvent />} />
           <Route path='/event/:id' element={<Event />} /> */}
@@ -25,4 +26,4 @@ function App() {
 }
 
 export default App;
-console.log('hi');
+console.log("hi");
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,17 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [userId, setUserId] = useState("")
+
+  // function postData() {
+  //   async function postUserData() {
+
+  //   };
+  //   const response = await fetch(
+  //     '',
+  //     {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify(),
+  //     }
+  //   );
+
+  function handleSubmit(e) {
+    e.preventdefault();
+  }
+
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+  console.log(setEmail);
+
+  function handleChangePassword(e) {
+    setPassword(e.target.value);
+  }
   return (
     <div>
       <h2>Event Planner App</h2>
       <form>
         {/* pass in appUserEmail to the API tbc, Get back appUserId - HandleClick for login*/}
 
-        <input placeholder='e-mail' />
+        <input
+          placeholder="e-mail"
+          value={email}
+          onChange={handleChangeEmail}
+        />
 
-        <input placeholder='password' />
-
-        <button>Login</button>
+        <input
+          placeholder="password"
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <Link to="/homepage">
+          <button>Login</button>
+        </Link>
 
         <button>Sign Up</button>
       </form>
