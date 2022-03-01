f3e48edaa4e99e5bc8fee36295e1c41fa5589bc8
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-03-01 15:41:24 +0000

    backgroundimage

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,10 +1,21 @@
-import React from "react";
-import Navbar from "../components/Nabvar";
+import React from 'react';
+import Navbar from '../components/Nabvar';
+//import event-background from '../images/event-background.png';
 
 function HomePage() {
   return (
     <div>
       <Navbar />
+      {/* <div>
+        <img src={eventbackground} alt='background' />
+      </div> */}
+
+      {/* <div
+        style={{ backgroundImage: `url(${eventbackground})` }}
+        alt='backgroundImage'
+      >
+        An image should be here
+      </div> */}
     </div>
   );
 }
