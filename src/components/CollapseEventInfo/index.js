e62fb5cbee5c3f1e9265055b570b72ae693bfbe3
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-03-02 16:11:22 +0000

    conditional render working

--- src/components/CollapseEventInfo/index.js
+++ src/components/CollapseEventInfo/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function CollapseEventInfo() {
+  // const [collapse, setCollapse] = useState(false);
+
+  // function collapseInfo(e) {
+  //   setCollapse(!collapse);
+  // }
+
   return <button>collapse event</button>;
 }
 
