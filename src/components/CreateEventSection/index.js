74b4e2fdf375387a23378804c4a42f66b793f624
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2022-03-03 09:43:25 +0000

    renamed event.css file to CreateEvent.css

--- src/components/CreateEventSection/index.js
+++ src/components/CreateEventSection/index.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import {
   DatePicker,
   Input,
@@ -9,25 +9,25 @@ import {
   Button,
   Space,
   message,
-} from "antd";
-import { UserOutlined } from "@ant-design/icons";
-import "antd/dist/antd.css";
-import "./CreateEventSection.css";
-import moment from "moment";
-import "moment/locale/zh-cn";
+} from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import './CreateEvent.css';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 
 //hexcode-safety-net-server.herokuapp.com"
 
 function CreateEventSection() {
   // States
   const [event, setEvent] = useState({
-    eventTitle: "",
+    eventTitle: '',
     people: [],
-    eventLocation: "",
-    eventDescription: "",
+    eventLocation: '',
+    eventDescription: '',
   });
-  const [eventDate, setEventDate] = useState("date pending");
-  const [eventTime, setEventTime] = useState("");
+  const [eventDate, setEventDate] = useState('date pending');
+  const [eventTime, setEventTime] = useState('');
   const [personMenu, setPersonMenu] = useState([]);
 
   function postData() {
@@ -39,16 +39,16 @@ function CreateEventSection() {
         eventLocation: event.eventLocation,
         eventDate: eventDate,
         eventTime: eventTime,
-        eventRequirements: "Booze",
-        eventCategory: "Drinks",
+        eventRequirements: 'Booze',
+        eventCategory: 'Drinks',
       };
       console.log(newEvent);
       const response = await fetch(
         // "https://hexcode-safety-net-server.herokuapp.com/events/",
         `https://hexcode-arrange-group-event.herokuapp.com/events/`,
         {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(newEvent),
         }
       );
@@ -97,103 +97,103 @@ function CreateEventSection() {
   const { TextArea } = Input;
   const menu = (
     <Menu onClick={handleMenuClick}>
-      <Menu.Item key="Belinda" icon={<UserOutlined />}>
+      <Menu.Item key='Belinda' icon={<UserOutlined />}>
         Belinda
       </Menu.Item>
-      <Menu.Item key="Luke" icon={<UserOutlined />}>
+      <Menu.Item key='Luke' icon={<UserOutlined />}>
         Luke
       </Menu.Item>
-      <Menu.Item key="James" icon={<UserOutlined />}>
+      <Menu.Item key='James' icon={<UserOutlined />}>
         James
       </Menu.Item>
     </Menu>
   );
 
   return (
-    <div className="createEventSection">
-      <h2 className="createEventTitle">Create an Event</h2>
-      <div className="formContainer">
+    <div className='createEventSection'>
+      <h2 className='createEventTitle'>Create an Event</h2>
+      <div className='formContainer'>
         <form>
-          <h3 className="inputTitle">Title</h3>
+          <h3 className='inputTitle'>Title</h3>
           <label>
             <Input
               maxLength={30}
-              placeholder="Set a title for your event"
-              name="eventTitle"
+              placeholder='Set a title for your event'
+              name='eventTitle'
               value={event.eventTitle}
               onChange={handleChange}
             />
           </label>
-          <h3 className="inputTitle">Location</h3>
+          <h3 className='inputTitle'>Location</h3>
           <label>
             <Input
               maxLength={40}
-              className="titleInput"
-              placeholder="Set a location for your event"
-              name="eventLocation"
+              className='titleInput'
+              placeholder='Set a location for your event'
+              name='eventLocation'
               value={event.eventLocation}
               onChange={handleChange}
             />
           </label>
-          <h3 className="inputTitle">People</h3>
-          <Space className="dropdownPeople" wrap>
+          <h3 className='inputTitle'>People</h3>
+          <Space className='dropdownPeople' wrap>
             <Input
-              className="inputedpeople"
+              className='inputedpeople'
               prefix={<UserOutlined />}
               value={personMenu}
             />
             <Dropdown.Button
-              className="addingpeople"
+              className='addingpeople'
               overlay={menu}
-              placement="bottomCenter"
+              placement='bottomCenter'
               icon={<UserOutlined />}
             >
               Add people
             </Dropdown.Button>
           </Space>
-          <h3 className="inputTitle">Date</h3>
+          <h3 className='inputTitle'>Date</h3>
           <label>
             <DatePicker
-              format="YYYY-MM-DD"
-              className="datePicker"
-              name="date"
+              format='YYYY-MM-DD'
+              className='datePicker'
+              name='date'
               onChange={onChange}
             />
           </label>
-          <h3 className="inputTitle">Time</h3>
+          <h3 className='inputTitle'>Time</h3>
           <label>
             <DatePicker
-              className="timePicker"
-              picker="time"
-              name="time"
+              className='timePicker'
+              picker='time'
+              name='time'
               onChange={onChangeTime}
             />
           </label>
-          <p className="disclaimer">
+          <p className='disclaimer'>
             <i>
-              {" "}
+              {' '}
               If you havent decided on a date or location dont worry, you can
               decide this later by adding a poll on the event and editing the
               event details once decided.
             </i>
           </p>
-          <h3 className="inputTitle">Description</h3>
+          <h3 className='inputTitle'>Description</h3>
           <label>
             <TextArea
               maxLength={255}
-              className="descriptionInput"
-              placeholder="Add a description for your event.."
+              className='descriptionInput'
+              placeholder='Add a description for your event..'
               autoSize
-              name="eventDescription"
+              name='eventDescription'
               value={event.eventDescription}
               onChange={handleChange}
             />
-            <div style={{ margin: "24px 0" }} />
+            <div style={{ margin: '24px 0' }} />
           </label>
-          <Link to="/homepage">
+          <Link to='/homepage'>
             <Button
-              className="createEventButton"
-              type="primary"
+              className='createEventButton'
+              type='primary'
               onClick={handleClick}
             >
               Create Event
