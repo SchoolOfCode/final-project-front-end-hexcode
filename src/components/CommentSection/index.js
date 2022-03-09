39addd9ad6bed0f0fa78ca805648821e2351a2d6
nicfheorais <7238289+nicfheorais@users.noreply.github.com>
2022-03-09 14:44:58 +0000

    Comments now rendering from database - fetch comments by event working

--- src/components/CommentSection/index.js
+++ src/components/CommentSection/index.js
@@ -18,6 +18,9 @@ import { API_URL } from "../../config/index.js";
 // import image9 from "../../images/9.png";
 
 function CommentSection({ loggedInUserId, eventId }) {
+    const USE_DB_COMMENTS = true; //use this to load the comments from database
+    //const USE_DB_COMMENTS = false; //use this to return to loading hard-coded comments
+
     // States
     const [comments, setComments] = useState(userComment);
     // setting new state to capture current event comments from database - so as to leave the hard coded comments functionality unchanged.
@@ -56,10 +59,21 @@ function CommentSection({ loggedInUserId, eventId }) {
                 `src/components/CommentSection/index.js: after fetch, data retrieved is: `
             );
             console.log(data);
-            const commentsObject = data.payload;
-            //TODO: think about if we need to check for a blank comments object. Epmty comments object IS NOT an error BUT we might want to set a flag for the render. Or on the render just check if (!commentsObject) - commentsObject woiuld need to be declared outside of useEffect if we were to use it elswhere.
 
-            setCommentsDb(commentsObject);
+            const commentsArrayFromDatabase = data.payload;
+            //TODO: think about if we need to check for a blank comments object. Epmty comments object IS NOT an error BUT we might want to set a flag for the render. Or on the render just check if (!commentsObjectFromDatabase) - commentsObjectFromDatabase woiuld need to be declared outside of useEffect if we were to use it elswhere.
+            // UPDATE - NO NEED - would just check the state variable, commentsDb intead:
+            //   if (!commentsDb) {
+            //       do stuff here for case where there are no comments
+            // } else {
+            //      go ahead and render the comments
+            // }
+            console.log(
+                `src/components/CommentSection/index.js: just before setting commentsDb state, commentsObjectFromDatabase is: ${commentsArrayFromDatabase} `
+            );
+
+            console.log({ commentsArrayFromDatabase });
+            setCommentsDb(commentsArrayFromDatabase);
         }
         //NB: ONLY attempt to fetch all comments for a given event IF that event id is a number (and not undefined or null)
         console.log(
@@ -117,57 +131,128 @@ function CommentSection({ loggedInUserId, eventId }) {
     // <Avatar style={{ backgroundColor: "green" }}></Avatar>;
 
     //TODO: create new version to return which will map over the new commentsDb state and will pass into the Comment component.
-    //  commentText
-    // commentDateTimePosted
-    // authorName
-    // authorUserId
-    return (
-        <>
-            <div className="comment-section" key="100">
-                <h4 key="200">Posts</h4>
-                <div className="comment-container" key="300">
-                    {comments.map((item, currIndex) => {
-                        return (
-                            <div className="comments" key={currIndex + "div"}>
-                                <Comment
-                                    key={currIndex}
-                                    author={item.author}
-                                    content={item.text}
-                                    avatar={
-                                        <ProfileImage
-                                            imageFileNumber={item.authorUserId}
-                                        />
-                                    }
-                                    datetime={item.datetime}
-                                />
+    //  commentsDb.commentText
+    // commentsDb.commentDateTimePosted
+    // commentsDb.authorName
+    // commentsDb.authorUserId
+    // commentsDb.commentId
+    //TEMP - using a big if-statement. when it's working, change to a ternary if staement around the comments map
+    if (USE_DB_COMMENTS) {
+        // use new return for the comments db -
+        console.log(
+            `src/components/CommentSection/index.js: just before render, commentsDb state is: ${commentsDb} `
+        );
+        console.log({ commentsDb });
+        return (
+            <>
+                <div className="comment-section" key="100">
+                    <h4 key="200">Posts</h4>
+                    <div className="comment-container" key="300">
+                        {!commentsDb ? (
+                            <div>
+                                Comments still being retrieved or not found...
                             </div>
-                        );
-                    })}
+                        ) : (
+                            commentsDb.map((commentsDb, currIndex) => {
+                                return (
+                                    <div
+                                        className="comments"
+                                        key={commentsDb.commentId + "div"}
+                                    >
+                                        <Comment
+                                            key={commentsDb.commentId}
+                                            author={commentsDb.authorName}
+                                            content={commentsDb.commentText}
+                                            avatar={
+                                                <ProfileImage
+                                                    imageFileNumber={
+                                                        commentsDb.authorUserId
+                                                    }
+                                                />
+                                            }
+                                            datetime={
+                                                commentsDb.commentDateTimePosted
+                                            }
+                                        />
+                                    </div>
+                                );
+                            })
+                        )}
+                    </div>
                 </div>
-            </div>
-            <div className="send-comment">
-                <input
-                    className="enter-comment"
-                    type="text"
-                    onChange={handleChange}
-                />
-                <button
-                    className="send-Btn"
-                    type="primary"
-                    onClick={handleClick}
-                >
-                    <BiSend />
-                </button>
-            </div>
-            <div className="empty-div"></div>
-        </>
-    );
+                <div className="send-comment">
+                    <input
+                        className="enter-comment"
+                        type="text"
+                        onChange={handleChange}
+                    />
+                    <button
+                        className="send-Btn"
+                        type="primary"
+                        onClick={handleClick}
+                    >
+                        <BiSend />
+                    </button>
+                </div>
+                <div className="empty-div"></div>
+            </>
+        );
+
+        //otherwise, if USE_DB_COMMENTS is false, then just print the exisciting hard-coded comments
+    } else {
+        return (
+            <>
+                <div className="comment-section" key="100">
+                    <h4 key="200">Posts</h4>
+                    <div className="comment-container" key="300">
+                        {comments.map((item, currIndex) => {
+                            return (
+                                <div
+                                    className="comments"
+                                    key={currIndex + "div"}
+                                >
+                                    <Comment
+                                        key={currIndex}
+                                        author={item.author}
+                                        content={item.text}
+                                        avatar={
+                                            <ProfileImage
+                                                imageFileNumber={
+                                                    item.authorUserId
+                                                }
+                                            />
+                                        }
+                                        datetime={item.datetime}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="send-comment">
+                    <input
+                        className="enter-comment"
+                        type="text"
+                        onChange={handleChange}
+                    />
+                    <button
+                        className="send-Btn"
+                        type="primary"
+                        onClick={handleClick}
+                    >
+                        <BiSend />
+                    </button>
+                </div>
+                <div className="empty-div"></div>
+            </>
+        );
+    }
 }
 
 export default CommentSection;
 
 //
-// c.comment_id as "commentId",
+//             c.comment_id as "commentId",
 //             c.comment_text as "commentText",
 //             to_char(c.comment_date_posted,'DD-MM-YYYY') as "commentDatePosted",
 //             to_char(c.comment_create_date_time,'DD-MM-YYYY HH:MM:SS') as "commentDateTimePosted",
@@ -232,22 +317,3 @@ export default CommentSection;
 //         </span>
 //     </Tooltip>,
 // ]}
-/* <div className="comment-container" key="300">
-{comments.map((item, currIndex) => {
-    return (
-        <div className="comments" key={currIndex + "div"}>
-            <Comment
-                key={currIndex}
-                author={item.author}
-                content={item.text}
-                avatar={
-                    <ProfileImage
-                        imageFileNumber={item.authorUserId}
-                    />
-                }
-                datetime={item.datetime}
-            />
-        </div>
-    );
-})}
-</div> */
